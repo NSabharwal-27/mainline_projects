a2ee08573845cacfce69ba6bee62eaf741f01b1b
sabharwal-nikhil-216240707 <nikhis345@gmail.com>
2022-01-26 23:23:14 -0500

    synced up versions from local copy to github

--- structs/singly-linked.c
+++ structs/singly-linked.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
 
 struct node{
 	int data;
@@ -8,9 +7,53 @@ struct node{
 };
 
 
-int main(int argc, const char* argv){
+void add_last(struct node *ptr, int data);
 
-	printf("Initializing linked list\n");	
+
+int main(int argc, const char* argv){
 	
+	printf("initializing linked list\n");
+	
+	struct node *head = NULL;
+	head = (struct node*) malloc(sizeof(struct node));	
+
+	printf("linked list has been initialized... \n"
+			"Populating linked list now!\n");
+
+	for(int i = 0; i < 32; i++){
+		add_last(head, i);
+	}
+
+	//print_List(head);
+
 	return 0;
 }
+
+
+void add_last(struct node *head, int data){
+
+	struct node *newNode = (struct node*)malloc(sizeof(struct node*));
+	struct node *tmp = NULL;
+
+	newNode->data = data;
+	newNode->next = head;
+
+	if(head == NULL){
+		head = newNode;
+		return;
+	} else{
+		tmp = head;
+
+		while(tmp->next != NULL){
+			tmp = tmp->next;
+		}// exits when tmp->next == null
+
+		tmp->next = newNode; // link to the list 
+	} 
+
+	free(tmp);
+	return;
+}
+
+
+
