2b0bdc75d99a640cd697bba3038e3c36e47e4289
sabharwal-nikhil-216240707 <nikhis345@gmail.com>
2022-01-27 01:46:04 -0500

    fixed another looping error

--- structs/singly-linked.c
+++ structs/singly-linked.c
@@ -8,6 +8,7 @@ struct node{
 
 
 void add_last(struct node **ptr, int data);
+void print_list(struct node *ptr);
 
 
 int main(int argc, const char* argv){
@@ -22,34 +23,42 @@ int main(int argc, const char* argv){
 
 	for(int i = 0; i < 32; i++){
 		add_last(&head, i);
-		printf("%d\n", i);
 	}
 
-	//print_List(head);
+	print_list(head);
 
 	return 0;
 }
 
+void print_list(struct node *head){
 
-void add_last(struct node **head, int data){
+	//struct node *temp = head;
+
+	while(head != NULL){
+		printf("%d\n", head->data);
+		head = head->next;
+	}
+}
+
+
+void add_last(struct node **head, int data){	
 
 	struct node *newNode = (struct node*)malloc(sizeof(struct node));
 
 	newNode->data = data;
 	newNode->next = NULL;
 
-	if(head == NULL){
+	if(*head == NULL){
 		*head = newNode;
-	} else {
-		struct node *tmp = *head;
-
-		while(tmp->next != NULL){
-			tmp = tmp->next;
-		}// exits when tmp->next == null
-
-		tmp->next = newNode; // link to the list 
+		return;
 	} 
+
+	struct node *tmp = *head;
+
+	while(tmp->next != NULL)
+		tmp = tmp->next;
+	// exits when tmp->next == null
+
+	tmp->next = newNode; // link to the list 
+	return;
 }
-
-
-
