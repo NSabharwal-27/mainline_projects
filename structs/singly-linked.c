34a53fa561a623b2c563ff7e9404468ee94c82ae
sabharwal-nikhil-216240707 <nikhis345@gmail.com>
2022-01-27 02:08:11 -0500

    removing a.out and updating singly-linked to match list.h

--- structs/singly-linked.c
+++ structs/singly-linked.c
@@ -1,64 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node{
-	int data;
-	struct node* next;
-};
-
-
-void add_last(struct node **ptr, int data);
-void print_list(struct node *ptr);
+#include "list.h"
 
 
 int main(int argc, const char* argv){
-	
+
+	int random = 0;
+	int size = 0; /* track the size of the linked list  need this for the merge sort command later */
 	printf("initializing linked list\n");
 	
-	struct node *head = NULL;
-	head = (struct node*) malloc(sizeof(struct node));	
+	struct node *head = (struct node*) malloc(sizeof(struct node));	
 
 	printf("linked list has been initialized... \n"
 			"Populating linked list now!\n");
 
-	for(int i = 0; i < 32; i++){
-		add_last(&head, i);
-	}
+
+
+	for(int i = 0; size <= 100; i++){
+		random = (rand() % 100);
+		add_last(&head, random);
+		size++;
+	} // terminates when the size of the list has reached max allowable size of the linked list
 
 	print_list(head);
 
 	return 0;
 }
 
-void print_list(struct node *head){
-
-	//struct node *temp = head;
-
-	while(head != NULL){
-		printf("%d\n", head->data);
-		head = head->next;
-	}
-}
-
-
-void add_last(struct node **head, int data){	
-
-	struct node *newNode = (struct node*)malloc(sizeof(struct node));
-
-	newNode->data = data;
-	newNode->next = NULL;
-
-	if(*head == NULL){
-		*head = newNode;
-		return;
-	} 
-
-	struct node *tmp = *head;
-
-	while(tmp->next != NULL)
-		tmp = tmp->next;
-	// exits when tmp->next == null
-
-	tmp->next = newNode; // link to the list 
-	return;
-}
